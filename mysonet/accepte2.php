c8f318a20a976297471f0ed2d70192a3f375cf42
CebrailDevOps <noobione-noobiwan@outlook.com>
2023-08-05 21:50:22 +0200

    cccc

--- mysonet/accepte2.php
+++ mysonet/accepte2.php
@@ -2,19 +2,21 @@
 if (isset($_GET['ref_demande'])) {
     include 'db.php';
     try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $stmt = $conn->prepare("SELECT demandeur, ip_demandeur FROM demandes_recues WHERE ref_demande = :ref_demande");
         $stmt->bindParam(':ref_demande', $ref_demande);
         $stmt->execute();
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
         $demandeur = $row['demandeur'];
         $ip_demandeur = $row['ip_demandeur'];
-        $stmt3 = $conn->prepare("DELETE FROM demandes_recues WHERE ref_demande = :ref_demande");
-        $stmt3->bindParam(':ref_demande', $ref_demande);
-        $stmt3->execute();
         if (isset($_GET['token'])) {
             $stmt4 = $conn->prepare("INSERT INTO mes_amis (pseudo,ip_add,token) VALUES (?, ?, ?)");
             $stmt4->execute([$demandeur, $ip_demandeur, $_GET['token']]);
         }
+        $stmt3 = $conn->prepare("DELETE FROM demandes_recues WHERE ref_demande = :ref_demande");
+        $stmt3->bindParam(':ref_demande', $ref_demande);
+        $stmt3->execute();
     } catch(PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
         exit(); // Arrêter l'exécution si la connexion échoue
