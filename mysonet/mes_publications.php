5db9e6d7996fbda7f879cb99cdff3282f1ae98b2
CebrailDevOps <noobione-noobiwan@outlook.com>
2023-08-09 10:09:19 +0200

    correctin mes_publications.php postes_amis.php

--- mysonet/mes_publications.php
+++ mysonet/mes_publications.php
@@ -71,7 +71,7 @@ $demandes_ami = $stmt3->fetchColumn();
 
 <div class="navbar">
     <a href="postes_amis.php">Publications des amis</a>
-    <a href="postes_amis.php">Mes publications</a>
+    <a href="mes_publications.php">Mes publications</a>
     <?php if ($demandes_ami > 0) {
         echo '<a href="notif.php" class="notif-link"><span class="notif-icon">🔔</span><span class="notif-count">'.$demandes_ami.'</span></a>';
     } ?>
--- mysonet/postes_amis.php
+++ mysonet/postes_amis.php
@@ -72,7 +72,7 @@ try {
 <div class="header"><?php echo $_SESSION['pseudo'] ?> - MySoNet.Online</div>
 
     <div class="navbar">
-        <a href="mes_publications.php">Publications des amis</a>
+        <a href="postes_amis.php">Publications des amis</a>
         <a href="mes_publications.php">Mes publications</a>
         <?php if ($demandes_ami > 0) {
             echo '<a href="notif.php" class="notif-link"><span class="notif-icon">🔔</span><span class="notif-count">'.$demandes_ami.'</span></a>';
