6c73190a9b16de41620c2005dcfed4500afed9cb
CebrailDevOps <noobione-noobiwan@outlook.com>
2023-08-24 10:54:30 +0200

    edit db.php

--- mysonet/db.php
+++ mysonet/db.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "123456a.";
+$password = "";
 $dbname = "mysonet";
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
